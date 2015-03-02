82ba910b7ade9c6a0187b459955a9ee116021819
blainekasten <blainekasten@gmail.com>
2015-03-02 16:33:30 -0600

    clean up logging

--- lib/polyfillrb.rb
+++ lib/polyfillrb.rb
@@ -6,6 +6,11 @@ module Polyfillrb
 
   def self.get_polyfills(ua, minify=false)
 
+    # we console.log the results of the polyfillService so that we
+    # can read them from the IO instance.
+    #
+    # This feels kind of hacky, so in the future we can look into
+    # better ways to inject JS into this.
     jscode = <<-JS
       console.log(
         require('#{PROJECT_DIRECTORY}/polyfill-service/lib').getPolyfillString({
@@ -16,14 +21,13 @@ module Polyfillrb
       );
     JS
 
+    # pump into a node runner
     i = IO.popen('node', 'r+')
     i.write(jscode)
     i.close_write
 
-    polyfills = i.read 
-    puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-    puts polyfills
-    polyfills
+    # return the results
+    return i.read 
   end
 
 
