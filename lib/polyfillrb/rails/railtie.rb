3e7cdaddb6faa7337959b8702774c76d0d132878
blainekasten <blainekasten@gmail.com>
2015-03-11 14:31:51 -0500

    attempt for concurrency

--- lib/polyfillrb/rails/railtie.rb
+++ lib/polyfillrb/rails/railtie.rb
@@ -15,17 +15,19 @@ module Polyfillrb
       config.to_prepare do
 
         # clone polyfill
-        if !::Polyfillrb.has_installed_service?
-          ::Rails.logger.info "Gathering Polyfill Library..."
-          %x( cd #{::Polyfillrb::PROJECT_DIRECTORY} && git clone git@github.com:Financial-Times/polyfill-service.git )
-        end
+        Thread.new do 
+          if !::Polyfillrb.has_installed_service?
+            ::Rails.logger.info "Gathering Polyfill Library..."
+            %x( cd #{::Polyfillrb::PROJECT_DIRECTORY} && git clone git@github.com:Financial-Times/polyfill-service.git )
+          end
 
-        if !::Polyfillrb.has_built_sources?
-          # build the npm locals
-          ::Rails.logger.info "Buidling Polyfills...\n  this may take a minute\n You're server may not respond until this is done"
-          %x( cd #{::Polyfillrb::PROJECT_DIRECTORY}/polyfill-service && npm install )
-          %x( cd #{::Polyfillrb::PROJECT_DIRECTORY}/polyfill-service && grunt buildsources )
-          ::Rails.logger.info "  Done! Polyfills are built. You're server should be back in action"
+          if !::Polyfillrb.has_built_sources?
+            # build the npm locals
+            ::Rails.logger.info "Buidling Polyfills...\n  this may take a minute\n You're server may not respond until this is done"
+            %x( cd #{::Polyfillrb::PROJECT_DIRECTORY}/polyfill-service && npm install )
+            %x( cd #{::Polyfillrb::PROJECT_DIRECTORY}/polyfill-service && grunt buildsources )
+            ::Rails.logger.info "  Done! Polyfills are built. You're server should be back in action"
+          end
         end
 
       end
